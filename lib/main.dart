beda8c43ef162b75a287d6b1e1b84937f92951e9
sakarpaudel <sakarpaudel06@gmail.com>
2024-01-17 17:57:46 +0545

    Depencencies: Resolve Geolocator

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+// main.dart
+
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(MyApp());
@@ -24,9 +28,10 @@ class WeatherApp extends StatefulWidget {
 }
 
 class _WeatherAppState extends State<WeatherApp> {
-  String apiKey = "YOUR_API_KEY";
+  String apiKey = "e583354804fb5ed82c9c19cf604f7b56"; // Replace with your actual API key
   Position? currentPosition;
   String currentLocation = "City, Country"; // Default location
+  dynamic weatherData;
 
   @override
   void initState() {
@@ -50,11 +55,24 @@ class _WeatherAppState extends State<WeatherApp> {
   }
 
   // Function to fetch weather data based on location
-  void _fetchWeatherData() {
-    // Implement the logic to fetch weather data from the API
-    // using the currentPosition.latitude and currentPosition.longitude
-    // and update the UI accordingly.
-    // You may use a package like http or dio for making API requests.
+  Future<void> _fetchWeatherData() async {
+    try {
+      String apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
+      String units = 'metric'; // You can change this to 'imperial' for Fahrenheit
+
+      final response = await http.get(Uri.parse(
+          '$apiUrl?lat=${currentPosition!.latitude}&lon=${currentPosition!.longitude}&appid=$apiKey&units=$units'));
+
+      if (response.statusCode == 200) {
+        setState(() {
+          weatherData = json.decode(response.body);
+        });
+      } else {
+        print("Error fetching weather data. Status code: ${response.statusCode}");
+      }
+    } catch (e) {
+      print("Error fetching weather data: $e");
+    }
   }
 
   @override
@@ -62,6 +80,8 @@ class _WeatherAppState extends State<WeatherApp> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Weather App'),
+        centerTitle: true,
+        backgroundColor: Colors.pink,
       ),
       body: Center(
         child: Column(
@@ -81,6 +101,20 @@ class _WeatherAppState extends State<WeatherApp> {
                 'Latitude: ${currentPosition!.latitude}\nLongitude: ${currentPosition!.longitude}',
                 style: TextStyle(fontSize: 16),
                 textAlign: TextAlign.center,
+              ),
+            SizedBox(height: 20),
+            if (weatherData != null)
+              Column(
+                children: [
+                  Text(
+                    'Weather: ${weatherData['weather'][0]['main']}',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  Text(
+                    'Temperature: ${weatherData['main']['temp']} °C',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ],
               ),
           ],
         ),
